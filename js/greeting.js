e150b8dfea75d3dde800d65741f988867ea84d03
Jonathan Sjelin <jsjelin@horizon.csueastbay.edu>
2018-04-24 18:12:20 -0700

    nav, index, css, also zurb app stuff

--- js/greeting.js
+++ js/greeting.js
@@ -3,11 +3,11 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18) {
-  greeting ='Good evening! Today is';
+  greeting ='The evening is upon us! Today is';
 } else if (hourNow >12) {
-greeting = 'Good afternoon! Today is';
+greeting = 'Hey, partner, its pizza time! Today is';
 } else if (hourNow > 0) {
-  greeting = 'Good morning! Today is';
+  greeting = 'Good morning, my friend! Today is';
 } else {
   greeting = 'Welcome';
 }
