a0b9213462267e7a3798f42f90d52d42a6e38e01
DanielleLowe <dlowe15@horizon.csueastbay.edu>
2018-04-29 01:47:55 -0700

    still working on getting the store hours js to work

--- js/storehours.js
+++ js/storehours.js
@@ -1,42 +1,52 @@
-var element = document.querySelectorAll('[data-target]')[0];
-var state = 'open';
+var now = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
-function changeState() {
-  state = 'open' === state ? 'closed' : 'open';
-  alert(state);
-}
+var checkTime = function() {
+  var today = weekday[now.getDay()];
+  var timeDiv = document.getElementById('timeDiv');
+  var dayOfWeek = now.getDay();
+  var hour = now.getHours();
+  var minutes = now.getMinutes();
 
-function runNext() {
-  var now = new Date();
-  var upcoming;
-  var nextChange;
-  var deltaDay = 0;
-  var deltaMS;
+  //add AM or PM
+  var suffix = hour >= 12 ? "PM" : "AM";
 
-  if ('open' === state) {
-    nextChange = element.getAttribute('data-close').split(':');
-  } else {
-    nextChange = element.getAttribute('data-open').split(':');
-    if (24 > now.getHours()) {
-      deltaDay = 1;
-    }
+  // add 0 to one digit minutes
+  if (minutes < 10) {
+    minutes = "0" + minutes
+  };
+
+  if ((dayOfWeek == 0 || dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3 || dayOfWeek == 4) && hour >= 11 && hour <= 21) {
+    hour = ((hour + 11) % 12 + 1); //i.e. show 1:15 instead of 13:15
+    timeDiv.innerHTML = 'it\'s ' + today + ' ' + hour + ':' + minutes + suffix + ' - we\'re open!';
+    timeDiv.className = 'open';
   }
 
-  upcoming = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate() + deltaDay,
-    nextChange[0],
-    nextChange[1],
-    0,
-    0
-  );
+  else if ((dayOfWeek == 5 || dayOfWeek == 6) && hour >= 11 && hour <= 22) {
+    hour = ((hour + 11) % 12 + 1);
+    timeDiv.innerHTML = 'it\'s ' + today + ' ' + hour + ':' + minutes + suffix + ' - we\'re open!';
+    timeDiv.className = 'open';
+  }
 
-  deltaMS =  upcoming - now;
+  else {
+    if (hour == 0 || hour > 21) {
+      hour = ((hour + 11) % 12 + 1); //i.e. show 1:15 instead of 13:15
+    }
+    timeDiv.innerHTML = 'It\'s ' + today + ' ' + hour + ':' + minutes + suffix + ' - we\'re closed!';
+    timeDiv.className = 'closed';
+  }
+};
 
-  setTimeout(function() {
-    changeState();
-  }, deltaMS);
-}
+var currentDay = weekday[now.getDay()];
+var currentDayID = "#" + currentDay; //gets todays weekday and turns it into id
+$(currentDayID).toggleClass("today"); //hightlights today in the view hours modal popup
 
-runNext();
+setInterval(checkTime, 1000);
+checkTime();
