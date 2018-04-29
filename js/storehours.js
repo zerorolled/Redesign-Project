b35396bf9ef6da7d169c1f8c5c8daed04e4ea6b9
DanielleLowe <dlowe15@horizon.csueastbay.edu>
2018-04-29 00:49:54 -0700

    attempting to get the open and close js

--- js/storehours.js
+++ js/storehours.js
@@ -1,105 +1,42 @@
+var element = document.querySelectorAll('[data-target]')[0];
+var state = 'open';
 
-///
-var today = new Date();
-var hourNow = today.getHours();
-var fremont;
-
-if (hourNow > 21) {
-  greeting ='Sorry, the Fremont store is closed.';
-} else if (hourNow >10) {
-greeting = 'The Fremont store is open.';
-//  } else if (hourNow > 0) {
-//    greeting = 'Good morning, my friend! Today is';
- // }
- else {
-  greeting = 'Welcome';
+function changeState() {
+  state = 'open' === state ? 'closed' : 'open';
+  alert(state);
 }
- document.getElementById('greeting');
 
-var elgreeting = document.getElementById('storehours');
+function runNext() {
+  var now = new Date();
+  var upcoming;
+  var nextChange;
+  var deltaDay = 0;
+  var deltaMS;
 
-elgreeting.innerHTML = fremont;
+  if ('open' === state) {
+    nextChange = element.getAttribute('data-close').split(':');
+  } else {
+    nextChange = element.getAttribute('data-open').split(':');
+    if (24 > now.getHours()) {
+      deltaDay = 1;
+    }
+  }
 
+  upcoming = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + deltaDay,
+    nextChange[0],
+    nextChange[1],
+    0,
+    0
+  );
 
+  deltaMS =  upcoming - now;
 
-// // check out http://www.w3schools.com/js/js_dates.asp
-//
-//   var d = new Date();
-//
-//   var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//
-// console.log("day number: " + d.getDay());
-//
-// console.log(day[d.getDay()]);
-//
-// console.log("month number: " + d.getMonth());
-//
-// var month = []; // modern way of writing: var month =new Array();
-// month[0] = "January";
-// month[1] = "February";
-// month[2] = "March";
-// month[3] = "April";
-// month[4] = "May";
-// month[5] = "June";
-// month[6] = "July";
-// month[7] = "August";
-// month[8] = "September";
-// month[9] = "October";
-// month[10] = "November";
-// month[11] = "December";
-//
-//
-// console.log(month[d.getMonth()]);
-//
-// console.log("hour: " + d.getHours());
-//
-// console.log("date: " + d.getDate());
-//
-// console.log(d.getFullYear());
-//
-// console.log(d.getMonth());
-//
-// console.log(d.toLocaleDateString());
-//
-// //change default 24 hour time to 12 hour time
-// var hour = (d.getHours());
-//
-// console.log(hour);
-//
-// var amPm = " ";
-//
-// if (hour >= 11){
-//   amPm = " pm";
-// }else {
-//   amPm = " am";
-// }
-// console.log(amPm);
-//
-//   if (hour === 0){
-//        hour = 12;
-//     }else if (hour >= 13){
-//        hour = hour - 12;
-//     }else if (hour >= 1 && hour <= 9 ) {
-//         hour = "0" + hour;
-// }
-// console.log(hour);
-//
-//
-// var n = day[d.getDay()];
-// var m = month[d.getMonth()];
-// var min = d.getMinutes();
-// var dateNum = d.getDate();
-// console.log(dateNum);
-//
-// if ( min >= 0 && min <= 9){
-//   min = "0" + min;
-// } else {
-//   min = min;
-// }
-// console.log (min);
-// //http://www.w3schools.com/jsref/met_document_getelementbyid.asp
-//
-// document.getElementById("today").innerHTML =
-//   n + ", " + dateNum +" " + m + " " + d.getFullYear();
-//
-// document.getElementById("time").innerHTML = hour + ":" + min + amPm;
+  setTimeout(function() {
+    changeState();
+  }, deltaMS);
+}
+
+runNext();
